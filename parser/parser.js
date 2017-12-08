51625149cbdb221fdc0021a35d2865ed368e2dd2
Evan <evan.graine@gmail.com>
2017-12-08 03:28:19 +0100

    Modified the parser to improve it and consider multiple alternatives for the same keyword and getting rid of parentheses.

--- parser/parser.js
+++ parser/parser.js
@@ -19,25 +19,49 @@ function parseText(text, keywords) {
         var word = words[i];
         console.debug("processing word : " + word);
         if (word.length <= 1) continue;
-        /* We remove question marks and set everything in lowercase*/
-        var w = word.toLowerCase();
-        while (w.indexOf("?") !== -1) {
-            w.remove(word.indexOf("?"));
-        }
 
+        /* Set everything in lowercase*/
+        var w = word.toLowerCase();
+
+        /* Get rid of question marks */
+        while (w.indexOf("?") !== -1) {
+            w = w.substring(0, w.length - 1);
+        }
+        console.debug("got rid of ? : " + w);
+        /* Get rid of points */
+        while (w.indexOf(".") !== -1) {
+            w = w.substring(0, w.length - 1);
+        }
+        console.debug("got rid of . : " + w);
+        /* Get rid of commas */
+        while (w.indexOf(",") !== -1) {
+            w = w.substring(0, w.length - 1);
+        }
+        console.debug("got rid of , : " + w);
         /* We search if it corresponds to a keyword */
         var index = -1;
+        var found = false;
         for(var j = 0; j < keywords.length; j++){
-
-            var keyword = keywords[j];
+            var keyword = keywords[j].split(',');
             console.debug("keyword : " + keyword);
-            if (keyword === w) {
-                index = j;
-                break;
+            for (var k = 0; k < keyword.length; k++){
+                finalkw = keyword[k];
+                /* Remove parenthesis for now */
+                if (finalkw[0] === "(") {
+                    finalkw = finalkw.substring(1, finalkw.length - 1);
+                }
+                console.debug("final keyword : " + finalkw);
+                if (finalkw === w) {
+                    index = j;
+                    found = true;
+                    break;
+                }
+                if (found) break;
             }
         }
 
         console.debug("index in keywords : " + index);
+
         /* If it exists we set the value to one */
         if (index !== -1) {
             validityArray[index] = 1;
@@ -46,4 +70,3 @@ function parseText(text, keywords) {
     console.debug("validity array : " + validityArray);
     return validityArray;
 }
-
